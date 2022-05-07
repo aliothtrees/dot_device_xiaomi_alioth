63343e70d6ba38ca42d82e6877dcf7f5afd8591d
StarkDroid <gamerprince.exp@gmail.com>
2022-05-07 05:06:06 +0000

    Init Corvus
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>

--- corvus_alioth.mk
+++ corvus_alioth.mk
@@ -9,24 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-# Boot resolution
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Quick tap
-TARGET_SUPPORTS_QUICK_TAP := true
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := corvus_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+CORVUS_MAINTAINER= Trishiraj
